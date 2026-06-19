0fc34913b9063b2c3095d7014e3c21132873c7ff
Vamsshi <thehyderabadnow@gmail.com>
2026-06-19 19:37:56 +0530

    theme issue fixed

---
+++ tailwind.config.ts
@@ -0,0 +1,16 @@
+// tailwind.config.ts
+import type { Config } from "tailwindcss";
+
+const config: Config = {
+  content: [
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/data/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/utils/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
+export default config;
